b7aa5d1f155ac1481c5befc816a1e6f8a839d5ce
madhav3059 <ms3059@srmist.edu.in>
2026-09-05 21:24:11 +0530

    Create BestTimeToBuyAndSellStock.java

---
+++ week4/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class BestTimeToBuyAndSellStock {
+
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int currentProfit = prices[i] - minPrice;
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+        for (int i = 0; i < n; i++) {
+            prices[i] = sc.nextInt();
+        }
+
+        System.out.println(maxProfit(prices));
+
+        sc.close();
+    }
+}
